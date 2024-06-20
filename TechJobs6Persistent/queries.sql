f78f6b619f1ced5ddb30a2ff067ae2b150c31fb2
Telly Pippa <telly@Tellys-PC.local>
2024-06-20 15:17:33 -0500

    finished SQL queries and assignment is done

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -6,7 +6,14 @@
 -- INT Id, LONGTEXT(VARCHAR) Name, and INT EmployerId
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
-
+-- SELECT *
+-- FROM Employers
+-- WHERE location = "St. Louis";
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
+-- SELECT Skills.SkillName
+-- FROM techjobs.Skills
+-- INNER JOIN techjobs.JobSkills ON Skills.Id = JobSkills.SkillsId
+-- WHERE JobSkills.JobsId = 8
+-- ORDER BY Skills.SkillName ASC;
